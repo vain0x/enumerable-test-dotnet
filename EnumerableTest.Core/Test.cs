49511e512ddb1636de125acf22d4759d2d3bbe70
vain0 <id_vain@yahoo.co.jp>
2016-10-27 00:59:17 +0900

    Add a comment for every public definition

--- EnumerableTest.Core/Test.cs
+++ EnumerableTest.Core/Test.cs
@@ -8,6 +8,12 @@ using System.Threading.Tasks;
 
 namespace EnumerableTest
 {
+    /// <summary>
+    /// Represents a result of a unit test.
+    /// <para lang="ja">
+    /// 単体テストの結果を表す。
+    /// </para>
+    /// </summary>
     public abstract class Test
     {
         internal string Name { get; }
@@ -58,11 +64,28 @@ namespace EnumerableTest
             return new AssertionTest(name, result);
         }
 
+        /// <summary>
+        /// Creates a passing unit test.
+        /// <para lang="ja">
+        /// 「正常」を表す単体テストの結果を生成する。
+        /// </para>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public static Test Pass(string name)
         {
             return OfAssertion(name, TrueAssertion.Instance);
         }
 
+        /// <summary>
+        /// Creates a violating unit test.
+        /// <para lang="ja">
+        /// 「違反」を表す単体テストの結果を生成する。
+        /// </para>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public static Test Violate(string name, string message)
         {
             return OfAssertion(name, new FalseAssertion(message));
@@ -80,21 +103,67 @@ namespace EnumerableTest
             return OfAssertion(name, new EqualAssertion(actual, target, expected, comparer));
         }
 
+        /// <summary>
+        /// Tests that two values are equal, using <paramref name="comparer"/>.
+        /// <para lang="ja">
+        /// <paramref name="comparer"/> で比較して、2つの値が等しいことを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="expected"></param>
+        /// <param name="actual"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
         public static Test Equal<X>(X expected, X actual, IEqualityComparer comparer)
         {
             return Equality(nameof(Equal), expected, actual, comparer, true);
         }
 
+        /// <summary>
+        /// Tests that two values are equal,
+        /// using <see cref="StructuralComparisons.StructuralEqualityComparer"/>.
+        /// <para lang="ja">
+        /// <see cref="StructuralComparisons.StructuralEqualityComparer"/> を使用して、
+        /// 2つの値が等しいことを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="expected"></param>
+        /// <param name="actual"></param>
+        /// <returns></returns>
         public static Test Equal<X>(X expected, X actual)
         {
             return Equal(expected, actual, StructuralComparisons.StructuralEqualityComparer);
         }
 
+        /// <summary>
+        /// Tests that two values are not equal, using <paramref name="comparer"/>.
+        /// <para lang="ja">
+        /// <paramref name="comparer"/> で比較して、2つの値が等しくないことを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="unexpected"></param>
+        /// <param name="actual"></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
         public static Test NotEqual<X>(X unexpected, X actual, IEqualityComparer comparer)
         {
             return Equality(nameof(NotEqual), unexpected, actual, comparer, false);
         }
 
+        /// <summary>
+        /// Tests that two values are not equal,
+        /// using <see cref="StructuralComparisons.StructuralEqualityComparer"/>.
+        /// <para lang="ja">
+        /// <see cref="StructuralComparisons.StructuralEqualityComparer"/> を使用して、
+        /// 2つの値が等しくないことを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="unexpected"></param>
+        /// <param name="actual"></param>
+        /// <returns></returns>
         public static Test NotEqual<X>(X unexpected, X actual)
         {
             return NotEqual(unexpected, actual, StructuralComparisons.StructuralEqualityComparer);
@@ -120,11 +189,30 @@ namespace EnumerableTest
             return SelectEquality(name, expected, source, f, comparer, true);
         }
 
+        /// <summary>
+        /// Tests that a value satisfies a predicate
+        /// <para lang="ja">
+        /// 値が条件を満たすことを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="X"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
         public static Test Satisfy<X>(X value, Expression<Func<X, bool>> predicate)
         {
             return SelectEqual(nameof(Satisfy), true, value, predicate);
         }
 
+        /// <summary>
+        /// Tests that an action throws an exception of type <typeparamref name="E"/>.
+        /// <para lang="ja">
+        /// アクションが型 <typeparamref name="E"/> の例外を送出することを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="f"></param>
+        /// <returns></returns>
         public static Test Catch<E>(Action f)
             where E : Exception
         {
@@ -140,6 +228,15 @@ namespace EnumerableTest
             }
         }
 
+        /// <summary>
+        /// Tests that a function throws an exception of type <typeparamref name="E"/>.
+        /// <para lang="ja">
+        /// 関数が型 <typeparamref name="E"/> の例外を送出することを検査する。
+        /// </para>
+        /// </summary>
+        /// <typeparam name="E"></typeparam>
+        /// <param name="f"></param>
+        /// <returns></returns>
         public static Test Catch<E>(Func<object> f)
             where E : Exception
         {
