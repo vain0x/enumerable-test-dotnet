22f97cee4260945bb430a06445a6c045459638cc
vain0 <id_vain@yahoo.co.jp>
2016-11-03 16:40:10 +0900

    Move code from Runner to Runner.Wpf.ViewModel

---
+++ EnumerableTest.Runner.Wpf.ViewModel/TestClassNode.fs
@@ -0,0 +1,69 @@
+﻿namespace EnumerableTest.Runner.Wpf
+
+open System.Collections.ObjectModel
+open DotNetKit.Observing
+open EnumerableTest.Runner
+
+type TestClassNode(name: string) =
+  let children =
+    ObservableCollection<TestMethodNode>([||])
+
+  let tryFindNode methodName =
+    children |> Seq.tryFind (fun ch -> ch.Name = methodName)
+
+  let testStatus = Uptodate.Create(NotCompleted)
+
+  let isPassed =
+    testStatus.Select
+      (function
+        | NotCompleted | Passed ->
+          true
+        | Violated | Error ->
+          false
+      )
+
+  member this.Children = children
+
+  member this.Name = name
+
+  member this.TestStatus = testStatus
+
+  member this.IsPassed = isPassed
+
+  member this.CalcTestStatus() =
+    let children = children |> Seq.toArray
+    let rec loop i current =
+      if i = children.Length then
+        current
+      else
+        let loop = loop (i + 1)
+        match (current, children.[i].TestStatus.Value) with
+        | (_, NotCompleted) | (NotCompleted, _) ->
+          NotCompleted
+        | (Error, _) | (_, Error) ->
+          Error |> loop
+        | (Violated, _) | (_, Violated) ->
+          Violated |> loop
+        | _ ->
+          Passed |> loop
+    loop 0 Passed
+
+  member this.Update(testMethodResults: array<string * obj>) =
+    let (existingNodes, newTestMethods) =
+      testMethodResults |> Seq.paritionMap
+        (fun (methodName, result) ->
+          match tryFindNode methodName with
+          | Some node -> (node, result) |> Some
+          | None -> None
+        )
+    let removedNodes =
+      children |> Seq.except (existingNodes |> Seq.map fst) |> Seq.toArray
+    for removedNode in removedNodes do
+      children.Remove(removedNode) |> ignore<bool>
+    for (methodName, result) in newTestMethods do
+      let node = TestMethodNode(methodName)
+      node.UpdateResult(result)
+      children.Add(node)
+    for (node, result) in existingNodes do
+      node.UpdateResult(result)
+    testStatus.Value <- this.CalcTestStatus()
---
+++ EnumerableTest.Runner.Wpf.ViewModel/TestTree.fs
@@ -0,0 +1,80 @@
+﻿namespace EnumerableTest.Runner.Wpf
+
+open System
+open System.Collections.ObjectModel
+open System.IO
+open System.Reflection
+open System.Threading
+open EnumerableTest.Runner
+open EnumerableTest.Sdk
+
+module Model =
+  let loadAssembly (assemblyName: AssemblyName) =
+    MarshalValue.Recursion <- 2
+    let assembly = Assembly.Load(assemblyName)
+    let testSuite = TestSuite.ofAssembly assembly
+    let results = ResizeArray()
+    let execution =
+      testSuite |> TestSuite.runAsync
+    execution |> Observable.subscribe
+      (fun testClassResult ->
+        results.Add(testClassResult |> TestClassResult.toSerializable)
+      )
+      |> ignore<IDisposable>
+    execution.Connect()
+    execution |> Observable.wait
+    results :> seq<_>
+
+type TestTree() =
+  let children = ObservableCollection<TestClassNode>()
+
+  let tryFindChild testClassFullName =
+    children |> Seq.tryFind (fun ch -> ch.Name = testClassFullName)
+
+  let disposables = ResizeArray<IDisposable>()
+
+  let context =
+    SynchronizationContext.Current
+
+  let watchAssemblyFile (load: unit -> unit) (file: FileInfo) =
+    let watcher = new FileSystemWatcher(file.DirectoryName, file.Name)
+    watcher.NotifyFilter <- NotifyFilters.LastWrite
+    watcher.Changed.Add(fun _ -> load ())
+    watcher.EnableRaisingEvents <- true
+    disposables.Add(watcher)
+
+  let updateResult result =
+    for (testClassFullName, testMethodResults) in result do
+      match tryFindChild testClassFullName with
+      | Some node ->
+        node.Update(testMethodResults)
+      | None ->
+        let node = TestClassNode(testClassFullName)
+        node.Update(testMethodResults)
+        children.Add(node)
+
+  member this.LoadAssemblyInNewDomain(assemblyName: AssemblyName) =
+    let domainName =
+      sprintf "EnumerableTest.Runner[%s]#%d" assemblyName.Name (Counter.generate ())
+    use runnerDomain =
+      AppDomain.create domainName
+    let result =
+      runnerDomain.Value |> AppDomain.run (fun () -> Model.loadAssembly assemblyName)
+    context.Send ((fun _ -> updateResult result), ())
+
+  member this.LoadFile(file: FileInfo) =
+    let assemblyName = AssemblyName.GetAssemblyName(file.FullName)
+    let load () = this.LoadAssemblyInNewDomain(assemblyName)
+    watchAssemblyFile load file
+    load ()
+
+  member this.Children =
+    children
+
+  member this.Dispose() =
+    for disposable in disposables do
+      disposable.Dispose()
+
+  interface IDisposable with
+    override this.Dispose() =
+      this.Dispose()
