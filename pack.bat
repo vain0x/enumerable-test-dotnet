b07b7dee238fa24ef687b298221765ed1588f57a
vain0 <id_vain@yahoo.co.jp>
2016-11-09 21:41:05 +0900

    Add pack.bat

---
+++ pack.bat
@@ -0,0 +1,1 @@
+".paket\paket" pack output nugets version %1 && ".paket\paket" push file nugets/EnumerableTest.Core.%1.nupkg && ".paket\paket" push file nugets/EnumerableTest.Runner.Console.%1.nupkg && ".paket\paket" push file nugets/EnumerableTest.Runner.Wpf.%1.nupkg && rmdir nugets /S /Q
