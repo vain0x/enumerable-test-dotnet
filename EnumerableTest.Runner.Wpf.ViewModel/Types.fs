b871fd04c9c9bd51308bae81de8cd5b241af0fd2
vain0 <id_vain@yahoo.co.jp>
2016-11-16 22:47:07 +0900

    Uninstall Argu from EnumerableTest.Runner.Wpf

--- EnumerableTest.Runner.Wpf.ViewModel/Types.fs
+++ EnumerableTest.Runner.Wpf.ViewModel/Types.fs
@@ -1,19 +1,8 @@
 ﻿namespace EnumerableTest.Runner.Wpf
 
 open System
-open Argu
 open Reactive.Bindings
 open EnumerableTest.Runner
-
-type AppArgument =
-  | [<MainCommand>]
-    Files of list<string>
-with
-  interface IArgParserTemplate with
-    member this.Usage =
-      match this with
-      | Files _ ->
-        "Paths to test assembly"
 
 type TestMethodResult =
   {
