2451c349353ea3570642b753b3749413669b650e
vain0 <id_vain@yahoo.co.jp>
2016-11-26 22:27:21 +0900

    Rename TestClass.runAsync to TestMethod.createManyAsync

--- EnumerableTest.Runner/TestMethod.fs
+++ EnumerableTest.Runner/TestMethod.fs
@@ -75,6 +75,30 @@ module TestMethod =
       Option.tryCatch (fun () -> instance |> Disposable.dispose)
     ofResult m.Name groupTest disposingError stopwatch.Elapsed
 
+  /// Builds computations to create TestMethod instance
+  /// for each test method from a test class type.
+  /// NOTE: Execute all computations to dispose created instances.
+  let createManyAsync (typ: Type) =
+    let methodInfos = typ |> TestClassType.testMethodInfos
+    let instantiate = typ |> TestClassType.instantiate
+    try
+      let computations =
+        methodInfos
+        |> Seq.map
+          (fun m ->
+            let instance = instantiate ()
+            let computation =
+              async {
+                return m |> create instance
+              }
+            (m, computation)
+          )
+        |> Seq.toArray
+      (computations, None)
+    with
+    | e ->
+      ([||], Some e)
+
   let isPassed (testMethod: TestMethod) =
     testMethod.Result.IsPassed
     && testMethod.DisposingError |> Option.isNone
