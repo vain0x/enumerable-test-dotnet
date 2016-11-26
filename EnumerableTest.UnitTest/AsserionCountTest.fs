34061588e635890e0bf67b5f9a0b77ae07298352
vain0 <id_vain@yahoo.co.jp>
2016-11-27 00:29:40 +0900

    Test AssertionCount module

---
+++ EnumerableTest.UnitTest/AsserionCountTest.fs
@@ -0,0 +1,68 @@
+﻿namespace EnumerableTest.UnitTest
+
+open Persimmon
+open Persimmon.Syntax.UseTestNameByReflection
+open EnumerableTest
+open EnumerableTest.Sdk
+open EnumerableTest.Runner
+
+module AsserionCountTest =
+  module test_ofAssertion =
+    let passedCase =
+      test {
+        do!
+          TrueAssertion()
+          |> AssertionCount.ofAssertion
+          |> assertEquals AssertionCount.onePassed
+      }
+
+    let violatedCase =
+      test {
+        do!
+          CustomAssertion(false, "violated", Seq.empty)
+          |> AssertionCount.ofAssertion
+          |> assertEquals AssertionCount.oneViolated
+      }
+
+  module test_ofGroupTest =
+    let normalCase =
+      test {
+        do!
+          [|
+            (0).Is(0)
+            (0).Is(1)
+          |].ToTestGroup("normal-group")
+          |> AssertionCount.ofGroupTest
+          |> assertEquals
+            {
+              TotalCount =
+                2
+              ViolatedCount =
+                1
+              ErrorCount =
+                0
+              NotCompletedCount =
+                0
+            }
+      }
+
+    let exceptionCase =
+      test {
+        do!
+          (seq {
+            yield (0).Is(1)
+            exn() |> raise
+          }).ToTestGroup("exception-group")
+          |> AssertionCount.ofGroupTest
+          |> assertEquals
+            {
+              TotalCount =
+                2
+              ViolatedCount =
+                1
+              ErrorCount =
+                1
+              NotCompletedCount =
+                0
+            }
+      }
