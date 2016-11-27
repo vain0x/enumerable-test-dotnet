bb9cca8b3ff89f3c69ac4aa4a2156feaaa54d315
vain0 <id_vain@yahoo.co.jp>
2016-11-27 22:26:39 +0900

    Add PermanentTestRunner

---
+++ EnumerableTest.Runner.Wpf.ViewModel/PermanentTestRunner.fs
@@ -0,0 +1,19 @@
+﻿namespace EnumerableTest.Runner.Wpf
+
+open System
+open System.IO
+
+type PermanentTestRunner() =
+  let testTree = new TestTree()
+
+  member this.TestTree = testTree
+
+  member this.LoadFile(file: FileInfo) =
+    testTree.LoadFile(file)
+
+  member this.Dispose() =
+    testTree.Dispose()
+
+  interface IDisposable with
+    override this.Dispose() =
+      this.Dispose()
