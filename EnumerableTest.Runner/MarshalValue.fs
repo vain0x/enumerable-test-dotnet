aeea3064ce5f27b53fca0599764179a10dc7292f
vain0 <id_vain@yahoo.co.jp>
2016-12-01 21:09:15 +0900

    Redefine MarshalValue.ofObj

--- EnumerableTest.Runner/MarshalValue.fs
+++ EnumerableTest.Runner/MarshalValue.fs
@@ -1,16 +1,24 @@
 ﻿namespace EnumerableTest.Runner
 
 open System
+open System.Collections
 open System.Collections.Generic
+open System.Reflection
 open Basis.Core
 
 type MarshalResult =
   | MarshalResult
     of Result<MarshalValue, exn>
 with
+  member this.Unwrap() =
+    let (MarshalResult value) = this
+    value
+
   member this.AsObject =
-    let (MarshalResult result) = this
-    result |> Result.toObj
+    this.Unwrap() |> Result.toObj
+
+  member this.ValueOrThrow =
+    this.Unwrap() |> Result.get
 
 and MarshalProperty =
   KeyValuePair<string, MarshalResult>
@@ -44,16 +52,102 @@ module MarshalValue =
         [||]
     }
 
-  let ofObj =
-    function
+  type private Factory =
+    {
+      Recursion:
+        int
+      Function:
+        int -> obj -> MarshalValue
+      Invoke:
+        obj -> MarshalValue
+    }
+  with
+    static member Create(recursion, f) =
+      {
+        Recursion = recursion
+        Function = f
+        Invoke = f recursion
+      }
+
+  let private create (factory: Factory) typ string properties =
+    {
+      TypeName =
+        typ |> Type.prettyName
+      String =
+        string
+      Properties =
+        if factory.Recursion < 0
+        then [||]
+        else properties |> Seq.toArray
+    }
+
+  let private getPublicInstancePropertyValues source =
+    seq {
+      let bindingFlags = BindingFlags.Instance ||| BindingFlags.Public
+      for propertyInfo in source.GetType().GetProperties(bindingFlags) do
+        let getter = propertyInfo.GetMethod
+        if getter |> isNull |> not
+          && propertyInfo.GetIndexParameters() |> Array.isEmpty
+        then
+          let result =
+            Result.catch (fun () -> getter.Invoke(source, [||]))
+            |> Result.mapFailure
+              (function
+                | :? TargetInvocationException as e -> e.InnerException
+                | e -> e
+              )
+          yield (propertyInfo, result)
+    }
+
+  let private publicProperties (factory: Factory) source =
+    seq {
+      for (propertyInfo, result) in source |> getPublicInstancePropertyValues do
+        let result = result |> Result.map factory.Invoke
+        yield KeyValuePair<_, _>(propertyInfo.Name, MarshalResult result)
+    }
+
+  let private ofCollection (factory: Factory) source =
+    let typ = source.GetType()
+    let collection = source |> Seq.cast
+    let count = collection |> Seq.length
+    let items =
+      seq {
+        for (i, x) in collection |> Seq.indexed do
+          yield (i, x |> factory.Invoke)
+      } |> Seq.cache
+    let string =
+      if count <= 10 then
+         items
+         |> Seq.map (fun (_, x) -> x.String)
+         |> String.concat ", "
+         |> sprintf "{%s}"
+        else
+          sprintf "{Count = %d}" count
+    let properties =
+      seq {
+        yield! publicProperties factory source
+        for (i, x) in items do
+          let key = sprintf "[%d]" i 
+          let value = x |> Success |> MarshalResult
+          yield KeyValuePair<_, _>(key, value)
+      }
+    create factory typ string properties
+
+  let private ofProperties factory (value: obj) =
+    let typ = value.GetType()
+    let properties = publicProperties factory value
+    create factory typ (value |> string) properties
+
+  let rec internal ofObjCore recursion value =
+    let factory =
+      Factory.Create(recursion - 1, ofObjCore)
+    match value with
     | null ->
       ofNull
+    | value when value.GetType() |> Type.isCollectionType ->
+      value :?> IEnumerable |> ofCollection factory
     | value ->
-      {
-        TypeName =
-          value.GetType().Name
-        String =
-          value.ToString()
-        Properties =
-          [||] // TODO:
-      }
+      value |> ofProperties factory
+
+  let rec ofObj value =
+    value |> ofObjCore Recursion
