64f1c268b4576fbfcde7e554fbd0a4767ca44505
vain0 <id_vain@yahoo.co.jp>
2016-12-31 21:17:12 +0900

    Add EnumerableTest.Runner.Wpf.UnitTest project

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-packages\Persimmon.Console\tools\Persimmon.Console.exe EnumerableTest.UnitTest\bin\Debug\EnumerableTest.UnitTest.dll
+packages\Persimmon.Console\tools\Persimmon.Console.exe EnumerableTest.UnitTest\bin\Debug\EnumerableTest.UnitTest.dll EnumerableTest.Runner.Wpf.UnitTest\bin\Debug\EnumerableTest.Runner.Wpf.UnitTest.dll
